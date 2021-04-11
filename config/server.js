68868dca574279074ad7a4610f5c0a3a0b18653c
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-04-11 19:41:45 +0530

    Status Update

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4c9f5dcd729faa53b4d2cf268bef5fa6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bf19961-4b53-4f10-b531-f5edf478df2c'
+};
