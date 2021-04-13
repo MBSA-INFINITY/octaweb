0cc119cbb67ff5d79d6989cea5d42e510df74688
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-04-13 19:27:23 +0530

    Status Update

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,22 @@
 module.exports = ({ env }) => {
-    if (env("NODE_ENV") === "production") {
-  return {
-    upload: {
-      provider: 'cloudinary',
+  const plugins = {
+    "github-publish": {
+      owner: "IET-NITK", // The gothub organisation or user
+      repo: "IET-NITK.github.io", // The name of the repository
+      workflow_id: "deploy.yml", // The workflow_id or filename
+      token: env("GITHUB_TOKEN"), // The GitHub personal access token with access to trigger workflows and view build status
+      branch: "daddy", // The branch the workflow should be triggered on
+    },
+  };
+  if (env("NODE_ENV") === "production") {
+    plugins["upload"]={
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      }
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
     }
   }
-    }
-  };
+  return plugins
+};
