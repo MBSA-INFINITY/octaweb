f73446435c1726ceae91e22c4d15ab65a328c693
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-04-14 14:00:56 +0530

    feat(about-club): about Club Schema

--- plugins/social-media/admin/src/containers/HomePage/index.js
+++ plugins/social-media/admin/src/containers/HomePage/index.js
@@ -1,19 +1,22 @@
-/*
- *
- * HomePage
- *
- */
+import { Button, Padded, Text } from "@buffetjs/core";
+import { Header } from "@buffetjs/custom";
+import { LoadingBar } from "@buffetjs/styles";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { memo, useState, useEffect, useRef } from "react";
+import { useGlobalContext, request } from "strapi-helper-plugin";
 
-import React, { memo } from 'react';
 // import PropTypes from 'prop-types';
 import pluginId from '../../pluginId';
 
 const HomePage = () => {
+  const { formatMessage } = useGlobalContext();
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Padded size="md" top left bottom right>
+
+      <h1>Social Media Management</h1>
+      This is currently only an idea: this page would serve as point through which the Social Media Managers would automatically be able to post to Twitter, LinkedIn, Facebook, Instagram and our IET-NITK Telegram channel, and get links (or even shortened links) to share through Whatsapp as well. If you like this idea, please inform Nirmal Khedkar.
+    </Padded>
   );
 };
 
