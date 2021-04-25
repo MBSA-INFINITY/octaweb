023e356391060cee2591ead0b47400e04973f797
GitHub <actions@circleci.com>
2021-04-26 00:19:19 +0530

    feat(blog): more columns in blog table

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -4,80 +4,82 @@
  *
  */
 /* eslint-disable */
-import React, { memo, useMemo } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { get, upperFirst } from 'lodash';
-import { auth, LoadingIndicatorPage } from 'strapi-helper-plugin';
-import PageTitle from '../../components/PageTitle';
-import { useModels } from '../../hooks';
+import React, { memo, useMemo } from "react";
+import { FormattedMessage } from "react-intl";
+import { get, upperFirst } from "lodash";
+import { auth, LoadingIndicatorPage } from "strapi-helper-plugin";
+import PageTitle from "../../components/PageTitle";
+import { useModels } from "../../hooks";
 
-import useFetch from './hooks';
-import { ALink, Block, Container, LinkWrapper, P, Wave, Separator } from './components';
-import SocialLink from './SocialLink';
+import useFetch from "./hooks";
+import {
+  ALink,
+  Block,
+  Container,
+  LinkWrapper,
+  P,
+  Wave,
+  Separator,
+  SocialLinkWrapper,
+} from "./components";
 
-const FIRST_BLOCK_LINKS = [
-  {
-    link:
-      'https://strapi.io/documentation/developer-docs/latest/getting-started/quick-start.html#_4-create-a-category-content-type',
-    contentId: 'app.components.BlockLink.documentation.content',
-    titleId: 'app.components.BlockLink.documentation',
-  },
-  {
-    link: 'https://github.com/strapi/foodadvisor',
-    contentId: 'app.components.BlockLink.code.content',
-    titleId: 'app.components.BlockLink.code',
-  },
-];
+import SocialLink from "./SocialLink";
 
 const SOCIAL_LINKS = [
   {
-    name: 'GitHub',
-    link: 'https://github.com/strapi/strapi/',
+    name: "GitHub",
+    link: "https://github.com/iet-nitk",
   },
   {
-    name: 'Slack',
-    link: 'https://slack.strapi.io/',
+    name: "MS Teams",
+    link: "https://slack.strapi.io/",
   },
   {
-    name: 'Medium',
-    link: 'https://medium.com/@strapi',
+    name: "Facebook",
+    link: "https://medium.com/@strapi",
   },
   {
-    name: 'Twitter',
-    link: 'https://twitter.com/strapijs',
+    name: "Twitter",
+    link: "https://medium.com/@strapi",
   },
   {
-    name: 'Reddit',
-    link: 'https://www.reddit.com/r/Strapi/',
+    name: "Instagram",
+    link: "https://twitter.com/strapijs",
   },
   {
-    name: 'Forum',
-    link: 'https://forum.strapi.io',
+    name: "Telegram",
+    link: "https://www.reddit.com/r/Strapi/",
   },
   {
-    name: 'Academy',
-    link: 'https://academy.strapi.io',
+    name: "Website",
+    link: "https://iet.nitk.ac.in",
   },
 ];
 
 const HomePage = ({ history: { push } }) => {
   const { error, isLoading, posts } = useFetch();
   // Temporary until we develop the menu API
-  const { collectionTypes, singleTypes, isLoading: isLoadingForModels } = useModels();
+  const {
+    collectionTypes,
+    singleTypes,
+    isLoading: isLoadingForModels,
+  } = useModels();
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     e.preventDefault();
 
     push(
-      '/plugins/content-type-builder/content-types/plugins::users-permissions.user?modalType=contentType&kind=collectionType&actionType=create&settingType=base&forTarget=contentType&headerId=content-type-builder.modalForm.contentType.header-create&header_icon_isCustom_1=false&header_icon_name_1=contentType&header_label_1=null'
+      "/plugins/content-type-builder/content-types/plugins::users-permissions.user?modalType=contentType&kind=collectionType&actionType=create&settingType=base&forTarget=contentType&headerId=content-type-builder.modalForm.contentType.header-create&header_icon_isCustom_1=false&header_icon_name_1=contentType&header_label_1=null"
     );
   };
 
   const hasAlreadyCreatedContentTypes = useMemo(() => {
-    const filterContentTypes = contentTypes => contentTypes.filter(c => c.isDisplayed);
+    const filterContentTypes = (contentTypes) =>
+      contentTypes.filter((c) => c.isDisplayed);
 
     return (
-      filterContentTypes(collectionTypes).length > 1 || filterContentTypes(singleTypes).length > 0
+      filterContentTypes(collectionTypes).length > 1 ||
+      filterContentTypes(singleTypes).length > 0
     );
   }, [collectionTypes, singleTypes]);
 
@@ -86,28 +88,28 @@ const HomePage = ({ history: { push } }) => {
   }
 
   const headerId = hasAlreadyCreatedContentTypes
-    ? 'HomePage.greetings'
-    : 'app.components.HomePage.welcome';
-  const username = get(auth.getUserInfo(), 'firstname', '');
+    ? "HomePage.greetings"
+    : "app.components.HomePage.welcome";
+  const username = get(auth.getUserInfo(), "firstname", "");
   const linkProps = hasAlreadyCreatedContentTypes
     ? {
-        id: 'app.components.HomePage.button.blog',
-        href: 'https://iet.nitk.ac.in/',
+        id: "app.components.HomePage.button.blog",
+        href: "https://iet.nitk.ac.in/",
         onClick: () => {},
-        type: 'blog',
-        target: '_blank',
+        type: "blog",
+        target: "_blank",
       }
     : {
-        id: 'app.components.HomePage.create',
-        href: '',
+        id: "app.components.HomePage.create",
+        href: "",
         onClick: handleClick,
-        type: 'documentation',
+        type: "documentation",
       };
 
   return (
     <>
       <FormattedMessage id="HomePage.helmet.title">
-        {title => <PageTitle title={title} />}
+        {(title) => <PageTitle title={title} />}
       </FormattedMessage>
       <Container className="container-fluid">
         <div className="row">
@@ -120,21 +122,28 @@ const HomePage = ({ history: { push } }) => {
                   name: upperFirst(username),
                 }}
               >
-                {msg => <h2 id="mainHeader">{msg}</h2>}
+                {(msg) => <h2 id="mainHeader">{msg}</h2>}
               </FormattedMessage>
               {hasAlreadyCreatedContentTypes ? (
                 <FormattedMessage id="app.components.HomePage.welcomeBlock.content.again">
-                  {msg => <P>Welcome to IET NITK's Official Content Management System (CMS). This will be the centrepoint of all things IET-NITK: Blogging, Project records, Events and you name it.. Have a fun time!</P>}
+                  {(msg) => (
+                    <P>
+                      Welcome to IET NITK's Official Content Management System
+                      (CMS). This will be the centrepoint of all things
+                      IET-NITK: Blogging, Project records, Events and you name
+                      it.. Have a fun time!
+                    </P>
+                  )}
                 </FormattedMessage>
               ) : (
                 <FormattedMessage id="HomePage.welcome.congrats">
-                  {congrats => {
+                  {(congrats) => {
                     return (
                       <FormattedMessage id="HomePage.welcome.congrats.content">
-                        {content => {
+                        {(content) => {
                           return (
                             <FormattedMessage id="HomePage.welcome.congrats.content.bold">
-                              {boldContent => {
+                              {(boldContent) => {
                                 return (
                                   <P>
                                     <b>{congrats}</b>&nbsp;
@@ -153,15 +162,17 @@ const HomePage = ({ history: { push } }) => {
               )}
               {hasAlreadyCreatedContentTypes && (
                 <div style={{ marginTop: isLoading ? 60 : 50 }}>
-                  We plan to show what content is editable by what role, so that you can conveniently contact the right person to make the change.
+                  We plan to show what content is editable by what role, so that
+                  you can conveniently contact the right person to make the
+                  change.
                 </div>
               )}
               <FormattedMessage id={linkProps.id}>
-                {msg => (
+                {(msg) => (
                   <ALink
                     rel="noopener noreferrer"
                     {...linkProps}
-                    style={{ verticalAlign: ' bottom', marginBottom: 5 }}
+                    style={{ verticalAlign: " bottom", marginBottom: 5 }}
                   >
                     Open IET-NITK Website
                   </ALink>
@@ -172,22 +183,40 @@ const HomePage = ({ history: { push } }) => {
 
           <div className="col-md-12 col-lg-4">
             <Block style={{ paddingRight: 30, paddingBottom: 0 }}>
-              <FormattedMessage id="HomePage.community">{msg => <h2>Follow IET-NITK</h2>}</FormattedMessage>
+              <FormattedMessage id="HomePage.community">
+                {(msg) => <h2>Follow IET-NITK</h2>}
+              </FormattedMessage>
               <FormattedMessage id="app.components.HomePage.community.content">
-                {content => <P style={{ marginTop: 7, marginBottom: 0 }}>We rely on each and every one of our members to advertise and promote our events. Please follow us, like and re-share our posts on the following social media platforms.</P>}
+                {(content) => (
+                  <P style={{ marginTop: 7, marginBottom: 0 }}>
+                    We rely on each and every one of our members to advertise
+                    and promote our events. Please follow us, like and re-share
+                    our posts on the following social media platforms.
+                  </P>
+                )}
               </FormattedMessage>
 
               <div
                 className="row social-wrapper"
                 style={{
-                  display: 'flex',
+                  display: "flex",
                   margin: 0,
                   marginTop: 36,
                   marginLeft: -15,
                 }}
               >
                 {SOCIAL_LINKS.map((value, key) => (
-                  <SocialLink key={key} {...value} />
+                  <>
+                    <SocialLinkWrapper key={key} className="col-6">
+                      <a
+                        href={value.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <span>{value.name}</span>
+                      </a>
+                    </SocialLinkWrapper>
+                  </>
                 ))}
               </div>
             </Block>
