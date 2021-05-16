9cc6c8d5c185fcbbbe2b15dc7c3832290451f191
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-05-16 23:34:07 +0530

    Status Update

--- plugins/mobile/controllers/mobile.js
+++ plugins/mobile/controllers/mobile.js
@@ -14,46 +14,38 @@ module.exports = {
    */
   tables: async (ctx) => {
     const rawBuilder = strapi.connections.default.raw(
-      "select * from core_store"
+      "select * from core_store;"
     );
     const response = await rawBuilder.then();
     const finalInformation = {}
-    response.map((e) =>
-      {
-        if(_.startsWith(e.key,"model_def_application::")){
-          const value=JSON.parse(e.value)
-          finalInformation[value.uid]={
-            ...finalInformation[value.uid],
-            collectionName: value.collectionName,
-            kind: value.kind,
-            name: value.info.name,
-            description: value.info.description,          
-          }
-        }
-        if(_.startsWith(e.key,
-          "plugin_content_manager_configuration_content_types::application::"
-        )){
-          const value=JSON.parse(e.value)
-          finalInformation[value.uid]={
-            ...finalInformation[value.uid],
-            defaultSortBy: value.settings.defaultSortBy,
-            mainField: value.settings.mainField,
-            defaultSortOrder: value.settings.defaultSortOrder,
-            layout: value.layouts.list
-          }
-        }
-      }
-    );
+    // response.map((e) =>
+    //   {
+    //     if(_.startsWith(e.key,"model_def_application::")){
+    //       const value=JSON.parse(e.value)
+    //       finalInformation[value.uid]={
+    //         ...finalInformation[value.uid],
+    //         collectionName: value.collectionName,
+    //         kind: value.kind,
+    //         name: value.info.name,
+    //         description: value.info.description,          
+    //       }
+    //     }
+    //     if(_.startsWith(e.key,
+    //       "plugin_content_manager_configuration_content_types::application::"
+    //     )){
+    //       const value=JSON.parse(e.value)
+    //       finalInformation[value.uid]={
+    //         ...finalInformation[value.uid],
+    //         defaultSortBy: value.settings.defaultSortBy,
+    //         mainField: value.settings.mainField,
+    //         defaultSortOrder: value.settings.defaultSortOrder,
+    //         layout: value.layouts.list
+    //       }
+    //     }
+    //   }
+    // );
     ctx.send({
-      message: finalInformation
-    });
-  },
-  index: async (ctx) => {
-    // Add your own logic here.
-    console.error("Heya!");
-    // Send 200 `ok`
-    ctx.send({
-      message: "mok",
+      message: response
     });
   },
 };
