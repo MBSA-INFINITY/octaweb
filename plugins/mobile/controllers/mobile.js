b97eaa3f4b509670cbfdc3d360db8725f6110e4b
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-05-16 23:45:37 +0530

    Status Update

--- plugins/mobile/controllers/mobile.js
+++ plugins/mobile/controllers/mobile.js
@@ -16,36 +16,39 @@ module.exports = {
     const rawBuilder = strapi.connections.default.raw(
       "select * from core_store;"
     );
-    const response = await rawBuilder.then();
+    let response = await rawBuilder.then();
+    if(process.env.NODE_ENV!=="development"){
+      response=response.rows;
+    }
     const finalInformation = {}
-    // response.map((e) =>
-    //   {
-    //     if(_.startsWith(e.key,"model_def_application::")){
-    //       const value=JSON.parse(e.value)
-    //       finalInformation[value.uid]={
-    //         ...finalInformation[value.uid],
-    //         collectionName: value.collectionName,
-    //         kind: value.kind,
-    //         name: value.info.name,
-    //         description: value.info.description,          
-    //       }
-    //     }
-    //     if(_.startsWith(e.key,
-    //       "plugin_content_manager_configuration_content_types::application::"
-    //     )){
-    //       const value=JSON.parse(e.value)
-    //       finalInformation[value.uid]={
-    //         ...finalInformation[value.uid],
-    //         defaultSortBy: value.settings.defaultSortBy,
-    //         mainField: value.settings.mainField,
-    //         defaultSortOrder: value.settings.defaultSortOrder,
-    //         layout: value.layouts.list
-    //       }
-    //     }
-    //   }
-    // );
+    response.map((e) =>
+      {
+        if(_.startsWith(e.key,"model_def_application::")){
+          const value=JSON.parse(e.value)
+          finalInformation[value.uid]={
+            ...finalInformation[value.uid],
+            collectionName: value.collectionName,
+            kind: value.kind,
+            name: value.info.name,
+            description: value.info.description,          
+          }
+        }
+        if(_.startsWith(e.key,
+          "plugin_content_manager_configuration_content_types::application::"
+        )){
+          const value=JSON.parse(e.value)
+          finalInformation[value.uid]={
+            ...finalInformation[value.uid],
+            defaultSortBy: value.settings.defaultSortBy,
+            mainField: value.settings.mainField,
+            defaultSortOrder: value.settings.defaultSortOrder,
+            layout: value.layouts.list
+          }
+        }
+      }
+    );
     ctx.send({
-      message: response
+      message: finalInformation
     });
   },
 };
